87893445a1580d79854c6fd9a2c4a34cd584eefa
chiharakenta <chiharakenta0519@gmail.com>
2020-08-13 11:50:10 +0900

    index create

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// ここから書き始める
+const db = require('./models/index');
+
+app.get('/', function(req, res) {
+  res.redirect('/todos');
+});
+
+app.get('/todos', function(req, res) {
+  db.todo.findAll().then(function(results) {
+    res.render('todos/index', { todos: results } );
+  });
+});
+
+app.post('/todos', function(req, res) {
+  const values = {
+    content: req.body.todoContent
+  };
+  db.todo.create(values).then(function(results) {
+    res.redirect('/todos');
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
