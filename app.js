3aa4ba390a531c2607a6f43d7005afe586fe6880
chiharakenta <chiharakenta0519@gmail.com>
2020-08-14 08:55:19 +0900

    change variables to constant variables

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,11 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ここから書き始める
 const db = require('./models/index');
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
-    var method = req.body._method
+    const method = req.body._method
     delete req.body._method
     return method
   }
