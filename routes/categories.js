024829f61b5d4899fec9eaeb6117144f15fd0723
chiharakenta <chiharakenta0519@gmail.com>
2020-08-14 14:41:53 +0900

    add categories index

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
 
-
+router.get('/', function(req, res) {
+  db.category.findAll().then(function(results) {
+    res.render('categories/index', { categories: results } );
+  });
+});
 
 module.exports = router;
