20c8c6bbaed3b12c57437e27f3b2439b4d101364
chiharakenta <chiharakenta0519@gmail.com>
2020-08-14 15:08:47 +0900

    add category crud

--- routes/categories.js
+++ routes/categories.js
@@ -8,4 +8,44 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/', function(req, res) {
+  const params = {
+    name: req.body.categoryName
+  };
+  db.category.create(params).then(function(results) {
+    res.redirect('/categories');
+  });
+});
+
+router.get('/:id/edit', function(req, res) {
+  db.category.findByPk(req.params.id).then(function(results) {
+    res.render('categories/edit.ejs', { category: results });
+  });
+});
+
+router.put('/:id', function(req, res) {
+  const params = {
+    name: req.body.categoryName
+  };
+  const filter = {
+    where: {
+      id: req.params.id
+    }
+  }
+  db.category.update(params, filter).then(function(results) {
+    res.redirect('/categories')
+  });
+})
+
+router.delete('/:id', function(req, res) {
+  const filter = {
+    where: {
+      id: req.params.id
+    }
+  };
+  db.category.destroy(filter).then(function(results) {
+    res.redirect('/categories');
+  });
+});
+
 module.exports = router;
